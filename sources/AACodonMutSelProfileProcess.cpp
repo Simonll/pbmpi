92c70fcdb28bb625a49b6211e5718807c313ddb9
sll <evol.simon@gmail.com>
2023-04-05 15:20:41 -0400

    small fix

--- sources/AACodonMutSelProfileProcess.cpp
+++ sources/AACodonMutSelProfileProcess.cpp
@@ -129,7 +129,6 @@ double AACodonMutSelProfileProcess::LogCodonProfilePrior()	{
 
 
 void AACodonMutSelProfileProcess::SampleCodonProfile()	{
-	double total = 0;
 	for (int i=0; i<statespace->GetNstate(); i++)  {
 		//codonprofile[i] = rnd::GetRandom().sExpo();
 		codonprofile[i] = 1.0/statespace->GetDegeneracy(i);
